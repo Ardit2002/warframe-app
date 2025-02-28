8430f3f387afc2725821184eba814aab0defdc61
unknown <ademiardit02@gmail.com>
2025-02-28 18:49:13 +0100

    Implementing user authentication feature

--- src/pages/interactive-map.js
+++ src/pages/interactive-map.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import FishingMap from '../components/FishingMap'; // Assuming FishingMap is your map component
+import FarmingMap from '../components/FarmingMap';
 
 function InteractiveMap() {
   return (
     <div>
-      <h1>Interactive Fishing & Mining Map</h1>
-      <FishingMap />
+      <h1>Interactive Farming Map</h1>
+      <FarmingMap />
     </div>
   );
 }
